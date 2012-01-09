1ec93b4246ed9eddc94554b250a798fc6a2fd2e5
david <s97113117.stu@gmail.com>
2012-01-10 03:11:08 +0800

    add homePage and homePageMapper instances

--- application/controllers/CrawlerController.php
+++ application/controllers/CrawlerController.php
@@ -2,9 +2,15 @@
 
 class CrawlerController extends My_Controller_Action
 {
+    protected $homePage;
+    protected $homePageMapper;
 
     public function init()
     {
+
+        $this->homePage = new Application_Model_HomePage();
+        $this->homePageMapper = new Application_Model_HomePageMapper();
+
     }
 
     public function indexAction()
