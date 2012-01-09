22db70409669b52c54ecb5c38fec56e62d5800b4
david <s97113117.stu@gmail.com>
2012-01-10 02:59:33 +0800

    add headScript to prepend pins.js

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,6 +5,10 @@ class IndexController extends My_Controller_Action
 
     public function init()
     {
+        parent::init();
+
+        $this->view->headScript()
+            ->prependFile('/static/js/pins.js');
     }
 
     public function indexAction()
