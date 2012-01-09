39af6bd1e02f8534df3c7e1de10d110ea9fdd96d
david <s97113117.stu@gmail.com>
2012-01-10 04:24:41 +0800

    set the Doctype to HTML5

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,6 +5,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
     protected function _initDoctype()
     {
+        $this->bootstrap('view');
+        $view = $this->getResource('view');
+        $view->doctype('HTML5');
     }
 
     protected function _initConfig()
