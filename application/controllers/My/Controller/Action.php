04a2e2803c52ff7f75c95e66221af6c61b9c6277
david <s97113117.stu@gmail.com>
2012-01-10 04:40:25 +0800

    add getDomQuery function

--- application/controllers/My/Controller/Action.php
+++ application/controllers/My/Controller/Action.php
@@ -8,5 +8,14 @@ abstract class My_Controller_Action extends Zend_Controller_Action
     {
     }
 
+    /* @param url   The URL contains the required HTML for dom query.
+     *
+     * @return      A Zend Dom Query instance.
+     */
+    public function getDomQuery($url)
+    {
+        $html = file_get_contents($url);
+        return new Zend_Dom_Query($html);
+    }
 
 }
