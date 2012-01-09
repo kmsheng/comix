530d9097f0467b305ee75ec6c0a441703d155c11
david <s97113117.stu@gmail.com>
2012-01-10 04:41:12 +0800

    add getChapterLink function

--- application/controllers/My/Controller/Action.php
+++ application/controllers/My/Controller/Action.php
@@ -18,4 +18,37 @@ abstract class My_Controller_Action extends Zend_Controller_Action
         return new Zend_Dom_Query($html);
     }
 
+    /* @param url   URL to fetch the link of each chapter.
+     *
+     * @return      An array contains chapter links.
+     */
+    public function getChapterLinks($url)
+    {
+        $dom = $this->getDomQuery($url);
+        $links = $dom->query('a');
+
+        preg_match('/\d+\.html/', $url, $number);
+
+        // Here's the base url to the intro page of comic.
+        $baseUrl = 'http://www.8comic.com/love/drawing-' . $number[0] . '?ch=';
+        $chapterLinks = array();
+
+        foreach ($links as $link) {
+
+            $onclick = $link->getAttribute('onclick');
+
+            // Regular expression for cview('7820-7.html',11);
+            preg_match('/^cview\(\'\d+-(\d+)\.html\'\,\d+\)/', $onclick, $chapter);
+
+            // If it fetches the chpater number
+            if (!empty($chapter)) {
+                $obj = new stdClass;
+                $obj->value = trim($link->nodeValue);
+                $obj->url = $baseUrl . $chapter[1];
+                $chapterLinks[] = $obj;
+            }
+        }
+
+        return $chapterLinks;
+    }
 }
