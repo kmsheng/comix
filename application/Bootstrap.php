b41e5139e4b129fa812a9b999325b6c75f16207c
david <s97113117.stu@gmail.com>
2012-01-10 04:23:52 +0800

    add Bootstrap.php

---
+++ application/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+
+    protected function _initDoctype()
+    {
+    }
+
+    protected function _initConfig()
+    {
+    }
+}
+
