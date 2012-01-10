c063deb7455d47071644e8ea7e2161dc26635581
david <s97113117.stu@gmail.com>
2012-01-10 14:52:24 +0800

    add function setDbTable

--- application/models/HomePageMapper.php
+++ application/models/HomePageMapper.php
@@ -4,4 +4,15 @@ class Application_Model_HomePageMapper
 {
     protected $_dbTable;
 
+    public function setDbTable($dbTable)
+    {
+        if (is_string($dbTable)) {
+            $dbTable = new $dbTable();
+        }
+        if (!$dbTable instanceof Zend_Db_Table_Abstract) {
+            throw new Exception('Invalid table data gateway provided');
+        }
+        $this->_dbTable = $dbTable;
+        return $this;
+    }
 }
